f97fbe588e38f5f486c384f9d802083ac05270e1
Mateusz <wszeborowskimateusz@gmail.com>
2020-05-12 15:11:50 +0200

    Turning off debug

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'fc@fe*jiik^!2x--bsskty*d4g+@33_7gf0^r96zf12#*j0$(f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
